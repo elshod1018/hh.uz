f3fbbcba58eb23abc5875b534f1b081268d88d74
elshod1018 <nuriddinovelshod2003@gmail.com>
2023-03-11 10:53:55 +0500

    initial

--- src/main/java/uz/hh/domain/Document.java
+++ src/main/java/uz/hh/domain/Document.java
@@ -1,4 +1,26 @@
 package uz.hh.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Document {
+    @Id
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid2")
+    private String id;
+    private String generatedFileName;
+    private String originalFileName;
+    private String mimeType;
+    private String filePath;
+    private Long fileSize;
+    private String extension;
 }
