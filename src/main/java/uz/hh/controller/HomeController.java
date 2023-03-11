89241c21e69926fa08f10c93c87efea306a67fa4
Infinity1441 <jumabekova.baxtigul@gmail.com>
2023-03-11 16:14:42 +0500

    1

---
+++ src/main/java/uz/hh/controller/HomeController.java
@@ -0,0 +1,40 @@
+package uz.hh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import uz.hh.config.security.UserSession;
+
+import java.util.Random;
+
+@Controller
+public class HomeController {
+
+    private final UserSession userSession;
+
+    public HomeController(UserSession userSession) {
+        this.userSession = userSession;
+    }
+
+    @GetMapping("/home")
+    public String hasAdminRole(Model model) {
+        //var s = "{bcrypt}$sdjvbhksnfbpgdhjnvlworhgeirnkvmd";
+        //var ss = "{noop}passwor";
+        //System.out.println("userSession.getUser().getId() = " + userSession.getId());
+        return "home";
+    }
+
+//    @GetMapping("/home2")
+//    public String homePage2() {
+//        if (new Random().nextBoolean()) {
+//            throw new CustomRuntimeException("Just For Fun Exception");
+//        }
+//        return "main2";
+//    }
+
+/*    @ExceptionHandler(CustomRuntimeException.class)
+    public String exception(Model model, CustomRuntimeException e) {
+        model.addAttribute("error", e.getMessage());
+        return "error";
+    }*/
+}
