9a766a74a8031aa989f483c553f6e3ac684ad774
Infinity1441 <jumabekova.baxtigul@gmail.com>
2023-03-12 10:15:39 +0500

    1

--- src/main/java/uz/hh/domain/User.java
+++ src/main/java/uz/hh/domain/User.java
@@ -13,15 +13,14 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Table(name = "users")
+@Entity(name = "users")
 @Builder
 public class User {
     @Id
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @GeneratedValue(generator = "uuid2")
     private String id;
-    @Column(nullable = false,name = "full_name")
+    @Column(nullable = false)
     private String fullName;
     @Column(nullable = false, unique = true)
     private String username;
@@ -31,6 +30,7 @@ public class User {
     private String password;
     @Column(nullable = false)
     @Builder.Default
+    @Enumerated(EnumType.STRING)
     private Status status = Status.NOT_ACTIVE;
 
     @Column(nullable = false)
@@ -42,16 +42,19 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
     )
     private Set<Role> roles;
+    @Column(nullable = false)
     @OneToMany(cascade = CascadeType.ALL,
-            mappedBy = "candidate",
             fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_chats"
+    )
     private Set<Chat> chats;
 
 
     @Builder.Default
     @Column(name = "is_deleted", nullable = false)
     private boolean is_deleted = false;
-    @Column(name = "created_at", nullable = false, columnDefinition = "timestamp default now()")
+    @Column(name = "created_at", nullable = false, columnDefinition = "default now()")
     @Builder.Default
     private LocalDateTime createdAt = LocalDateTime.now();
 
