d8f74e68e0999080776768d507480da5add8b2cb
elshod1018 <nuriddinovelshod2003@gmail.com>
2023-03-12 12:19:15 +0500

    my commit

---
+++ src/main/java/uz/hh/repository/ChatRepository.java
@@ -0,0 +1,10 @@
+package uz.hh.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.hh.domain.Chat;
+
+import java.util.List;
+
+public interface ChatRepository extends JpaRepository<Chat, String> {
+    List<Chat> findAllById(String id);
+}
